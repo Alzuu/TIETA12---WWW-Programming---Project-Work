9a20ac4cab215a1f5094f7ccd9d5c7149e332f6a
Allan Li <allan.li@tuni.fi>
2020-03-04 16:34:56 +0200

    5: changed type of number and CVC

--- server/models/CreditCard.js
+++ server/models/CreditCard.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 
 const creditCardSchema = mongoose.Schema({
   number: {
-    type: Number,
+    type: String,
     required: true,
     unique: true,
+    trim: true,
+    minlength: 1,
+    maxlength: 20,
   },
   CVC: {
-    type: Number,
+    type: String,
     required: true,
+    trim: true,
+    minlength: 3,
+    maxlength: 4,
   },
   ownerName: {
     type: String,
