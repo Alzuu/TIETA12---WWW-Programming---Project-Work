598d0ac3c6b1500941ba9f44034855287529f0f5
Allan Li <allan.li@tuni.fi>
2020-03-04 16:45:58 +0200

    12: added credit card controller

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -6,9 +6,9 @@ const creditCardsRoutes = require('./creditCardsRoutes');
 
 const router = express.Router();
 
-router.use('/', (req, res) => {
-  res.json('API routes');
-});
+// router.use('/', (req, res) => {
+//   res.json('API routes');
+// });
 router.use('/users', usersRoutes);
 router.use('/items', itemsRoutes);
 router.use('/bankaccounts', bankAccountsRoutes);
