f7ef9e453d357ab0696694ddda1493092fce5bce
Kari Harmaahieta <kari.harmaahieta@tuni.fi>
2020-03-05 09:50:58 +0000

    itemController first version ready

--- server/routes/itemsRoutes.js
+++ server/routes/itemsRoutes.js
@@ -2,17 +2,18 @@ const express = require('express');
 const itemController = require('../controllers/itemController');
 const verifyToken = require('../authentication/verifyToken');
 const getRole = require('../authentication/getRole');
+
 const router = express.Router();
 
-router.get('/customers', itemController.listCustomersItems);
+router.get('/shopkeepers', itemController.listShopkeepersItems);
 router.use((req, res, next) => verifyToken(req, res, next));
 router.use((req, res, next) => getRole(req, res, next));
 router.get('/', itemController.listAll);
-router.get('/shopkeepers', itemController.listShopkeepersItems);
+router.get('/customers', itemController.listCustomersItems);
 router.post('/', itemController.add);
 router.get('/:id', itemController.getOne);
-router.put('/:id');
-router.delete('/:id');
-router.put('/:id/sell/:userid');
+router.put('/:id', itemController.updateOne);
+router.delete('/:id', itemController.deleteOne);
+router.put('/:id/sell/:userid', itemController.sellOne);
 
 module.exports = router;
