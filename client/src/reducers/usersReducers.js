fc84bc52fca5c6d5448b82ee3d83dd3d4a36ea26
Kari Harmaahieta <kari.harmaahieta@tuni.fi>
2020-03-16 06:12:07 +0000

    Fixed state: user is null

--- client/src/reducers/usersReducers.js
+++ client/src/reducers/usersReducers.js
@@ -1,30 +1,30 @@
 export function userLoginHasErrored(state = false, action) {
-    switch (action.type) {
-        case 'USER_HAS_ERRORED':
-            return action.loginHasErrored;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'USER_HAS_ERRORED':
+      return action.loginHasErrored;
+    default:
+      return state;
+  }
 }
 
 export function userIsLoading(state = false, action) {
-    switch (action.type) {
-        case 'USER_IS_LOADING':
-            return action.isLoading;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'USER_IS_LOADING':
+      return action.isLoading;
+    default:
+      return state;
+  }
 }
 
 export function user(state = [], action) {
-    switch (action.type) {
-        case 'USER_FETCH_DATA_SUCCESS':
-            return action.user;
-        case 'USER_FETCH_DATA_FAILURE':
-            return null;
-        case 'USER_HAS_LOGGED_OUT':
-            return null;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'USER_FETCH_DATA_SUCCESS':
+      return action.user;
+    case 'USER_FETCH_DATA_FAILURE':
+      return false;
+    case 'USER_HAS_LOGGED_OUT':
+      return false;
+    default:
+      return state;
+  }
 }
