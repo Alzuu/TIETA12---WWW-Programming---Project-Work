245a5bc7cd031aa0c89ad4bc6f1e6f649cb3f849
Kari Harmaahieta <kari.harmaahieta@tuni.fi>
2020-03-19 13:41:08 +0000

    Added new Material UI layout and navigation

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
-import Box from '@material-ui/core/Box';
+import Navigation from './Navigation';
+import Container from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 
 class Layout extends Component {
   render() {
     return (
-      <>
-        <Header />
-        <Box>{this.props.children}</Box>
-      </>
+      <Container className="rootContainer">
+        <Navigation />
+        <Paper style={{ margin: 10, paddingBottom: 10 }} square elevation={3}>
+          {this.props.children}
+        </Paper>
+      </Container>
     );
   }
 }
---
+++ client/src/components/theme.js
@@ -0,0 +1,14 @@
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#81c784',
+    },
+    secondary: {
+      main: '#ff8a80',
+    },
+  },
+});
+// '#81c784'
+export default theme;
