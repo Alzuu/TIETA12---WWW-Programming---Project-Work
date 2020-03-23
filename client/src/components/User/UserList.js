836a8f012a9a0ba86a2b97a6df6e649cac29900c
ps97676 <peter.skogberg@tuni.fi>
2020-03-23 09:57:13 +0200

    56: added pagination to User List and some material ui to editable user page

--- client/src/components/User/UserList.js
+++ client/src/components/User/UserList.js
@@ -16,6 +16,8 @@ import { userModify } from '../../actions/usersActions';
 
 const UserList = (props) => {
     const [users, setUsers] = useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
     useEffect(() => {
         getUsers();
@@ -30,42 +32,76 @@ const UserList = (props) => {
             })
     );
 
-    return (
-        <>
-            <h1>User List</h1>
-            <TableContainer>
-                <Table size="small">
-                    <TableHead>
-                    <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Role</TableCell>
-                        <TableCell>Bank Account ID</TableCell>
-                        <TableCell>Credit Card ID</TableCell>
-                        <TableCell>Edit</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {users.map((user) => 
-                    <TableRow key={user._id}>
-                        <TableCell>{user.name}</TableCell>
-                        <TableCell>{user.role}</TableCell>
-                        <TableCell>{user.bankAccountId}</TableCell>
-                        <TableCell>{user.creditCardId}</TableCell>
-                        <TableCell>                        
-                            <Link to={`/users/adminEdit/${user._id}`}>
-                                <IconButton color="primary">
-                                    <EditIcon />
-                                </IconButton>
-                            </Link>
-                        </TableCell>
-                    </TableRow>
-            )}
-                </TableBody>
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
 
-        </Table>
-      </TableContainer>
-        </>
-    );
+      const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+      };
+
+
+      if (!users) {
+        return <></>;    
+      }
+    if (users.length > 0) {
+        const emptyRows =
+        rowsPerPage -
+        Math.min(rowsPerPage, users.length - page * rowsPerPage);
+
+        return (
+            <>
+                <h1>User List</h1>
+                <TableContainer>
+                    <Table size="small">
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Role</TableCell>
+                            <TableCell>Bank Account ID</TableCell>
+                            <TableCell>Credit Card ID</TableCell>
+                            <TableCell>Edit</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {users
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((user) => 
+                        <TableRow key={user._id}>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.role}</TableCell>
+                            <TableCell>{user.bankAccountId}</TableCell>
+                            <TableCell>{user.creditCardId}</TableCell>
+                            <TableCell>                        
+                                <Link to={`/users/adminEdit/${user._id}`}>
+                                    <IconButton color="primary">
+                                        <EditIcon />
+                                    </IconButton>
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                )}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={5}
+                            count={users.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                    </TableRow>
+                </TableFooter>
+            </Table>
+          </TableContainer>
+            </>
+        );
+    }
+    return <></>;    
 }
 /*
 <Link to={}>edit {user.name}</Link>
