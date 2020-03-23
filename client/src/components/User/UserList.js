caddfc533215a7dc512ba445fc957d99c42227bd
ps97676 <peter.skogberg@tuni.fi>
2020-03-23 09:24:40 +0200

    56: added material ui to User List

--- client/src/components/User/UserList.js
+++ client/src/components/User/UserList.js
@@ -1,7 +1,17 @@
 import React, { Component, useEffect, useState } from 'react'
 
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom'
+import EditIcon from '@material-ui/icons/Edit';
+import IconButton from '@material-ui/core/IconButton';
+import {Link} from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableFooter from '@material-ui/core/TableFooter';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
 import { userModify } from '../../actions/usersActions';
 
 const UserList = (props) => {
@@ -23,15 +33,44 @@ const UserList = (props) => {
     return (
         <>
             <h1>User List</h1>
-            {users.map((user) => 
-                <>
-                    <Link to={`/users/adminEdit/${user._id}`}>edit {user.name}</Link>
-                    <br />
-                </>
+            <TableContainer>
+                <Table size="small">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Role</TableCell>
+                        <TableCell>Bank Account ID</TableCell>
+                        <TableCell>Credit Card ID</TableCell>
+                        <TableCell>Edit</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {users.map((user) => 
+                    <TableRow key={user._id}>
+                        <TableCell>{user.name}</TableCell>
+                        <TableCell>{user.role}</TableCell>
+                        <TableCell>{user.bankAccountId}</TableCell>
+                        <TableCell>{user.creditCardId}</TableCell>
+                        <TableCell>                        
+                            <Link to={`/users/adminEdit/${user._id}`}>
+                                <IconButton color="primary">
+                                    <EditIcon />
+                                </IconButton>
+                            </Link>
+                        </TableCell>
+                    </TableRow>
             )}
+                </TableBody>
+
+        </Table>
+      </TableContainer>
         </>
     );
 }
+/*
+<Link to={}>edit {user.name}</Link>
+                    <br />
+*/
 
 const mapStateToProps = (state) => {
   return {
