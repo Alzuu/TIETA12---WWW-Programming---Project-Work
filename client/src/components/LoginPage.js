6349111f724b39051d89e08ab48033c8b52840db
ps97676 <peter.skogberg@tuni.fi>
2020-03-23 10:18:15 +0200

    56: added material ui to login page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,25 +1,37 @@
 import React, { Component, useEffect, useState } from 'react'
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { userFetchData, userFetchHasErrored } from '../actions/usersActions';
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+import FormGroup from '@material-ui/core/FormGroup';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import UpdateIcon from '@material-ui/icons/Update';
+
 const LoginPage = (props) => {
     const [loginHasBeenAttempted, setLoginHasBeenAttempted] = useState(false);
     const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+    const [userPassword, setUserPassword] = useState('');
 
     useEffect(() => {
         props.userFetchHasErrored(false);
-      }, []);
+    }, []);
 
-    const login = () => {
-        props.fetchData(userName, password);
+      const handleUserNameChange = (e) => { setUserName(e.target.value) }
+      const handleUserPasswordChange = (e) => { setUserPassword(e.target.value) }
+
+      const login = () => {
+        props.fetchData(userName, userPassword);
         setLoginHasBeenAttempted(true);
     }
 
     if (loginHasBeenAttempted && !props.loginHasErrored && !props.isLoading) {
         return <Redirect to={'/'} />;
     }
+    /*
     return (
         <div className="container">
             <h1>Login Page</h1>
@@ -39,6 +51,52 @@ const LoginPage = (props) => {
             </div>
         </div>
     );
+    */
+   return (
+    <Box className="addItemBox">
+    <Typography variant="h2">Edit user</Typography>
+      <form className="addItemBox">
+      <TextField
+                label='Name'
+                type='text'
+                name='name'
+                minLength={1}
+                maxLength={10}
+                required
+                value={userName}
+                onChange={handleUserNameChange}
+                on
+              />
+                        <TextField
+                label='Password'
+                type='password'
+                name='password'
+                minLength={1}
+                maxLength={10}
+                required
+                value={userPassword}
+                onChange={handleUserPasswordChange}
+                on
+              />
+
+<FormGroup row={true}>
+                <Button
+                  type="button"
+                  color="primary"
+                  variant="outlined"
+                  onClick={login}
+                  startIcon={<UpdateIcon />}
+                >
+                    Login
+                </Button>
+                <div>
+                    {props.loginHasErrored && <div><br /><p>Wrong username or password</p></div>}
+                </div>
+              </FormGroup>
+            </form>
+          </Box>
+
+   );
 }
 
 const mapStateToProps = (state) => {
